4447276ff8abb9f119361525c9d7e25948cd4bca
Paola Garbato <paolagarbato@gmail.com>
2020-08-21 14:48:26 -0300

    CONFERINDO SE USUÁRIO E SENHA SAO IGUAIS A ALGUM REGISTRADO NO BANCO DE DADOS =DDDD

--- Formulário/Form1.cs
+++ Formulário/Form1.cs
@@ -8,14 +8,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Formulário
 {
    
     public partial class Form1 : Form
     {
-        string confirmarUsuario, confirmarSenha;
-
         public Form1()
         {
             InitializeComponent();
@@ -31,16 +30,7 @@ namespace Formulário
 
         public void btnIniciar_Click(object sender, EventArgs e)
         {
-            string usuario = txtUsuario.Text;
-            string senha = txtSenha.Text;
-
-           if (usuario == confirmarUsuario && senha == confirmarSenha)
-            {
-                MessageBox.Show("Login realizado com sucesso!", "Login realizado", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            } else
-            {
-                MessageBox.Show("Dados incorretos.", "Erro.", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }        
+            Login();
         }
 
         private void lblCadastrar_Click(object sender, EventArgs e)
@@ -48,6 +38,39 @@ namespace Formulário
             Cadastro cadastro = new Cadastro();
             cadastro.Show();
             this.Visible = false;
+        }
+
+        public void Login()
+        {
+            string usuario = txtUsuario.Text;
+            string senha = txtSenha.Text;
+            int i = 0;
+            SqlConnection connection = new SqlConnection("Data Source=LAPTOP-LBQF810B;Initial Catalog=Formulario;Integrated Security=True");
+
+            string sql = "SELECT COUNT(Usuario) FROM Usuario WHERE Usuario = @usuario AND Senha = @senha";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+
+            cmd.Parameters.Add("@usuario", SqlDbType.VarChar).Value = usuario;
+            cmd.Parameters.Add("@senha", SqlDbType.VarChar).Value = senha;
+
+            connection.Open();
+
+            i = (int)cmd.ExecuteScalar();
+            
+            if(i == 1)
+            {
+                MessageBox.Show("Login realizado com sucesso!", "Login realizado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Dados incorretos.", "Erro.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+              
+
+
+
+
+        connection.Close();
         }
     }
 }
